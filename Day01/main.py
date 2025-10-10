90c488a4dbc830038ea822a2895e7606f67232b7
TungPhan2808 <phanmanhtung123@gmail.com>
2025-10-10 23:37:00 +0700

    Variable

---
+++ Day01/main.py
@@ -0,0 +1,52 @@
+# print("Hello Pyhon")
+#
+# first_name = "Hasada"
+# last_name = "Shito"
+# full_name = first_name + " " + last_name
+# print("Hello " + full_name)
+# # print(type(name))
+#
+# age = 18
+# age += 1
+# print("Your age is " + str(age))
+# print(type(age))
+#
+# height = 250.23
+# print(type(height))
+#
+# human = False
+# print(type(human))
+
+#Multiple assignment
+
+# name, age, human = "Tung", 21, True
+#
+# Tien = Linh = Hai = Nhu = 30
+
+#
+name = "Tung lun"
+age = "21"
+print(len(name))
+print(name.find("n"))
+print(name.capitalize())
+print(name.upper())
+print(name.isdigit())
+print(age.isdigit())
+print(name.isalpha())
+print(name.count("u"))
+print(name.replace("n","p"))
+print(name*3)
+
+#type casting
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+x = float(x)
+y = str(y)
+z = float(z) +1
+
+print(x)
+print(y)
+print(z*3)
+
